0a463676bb6f08ad75bccaa934e60c7cc884c640
Nikos Ioannidis <nikos@antmoves.com>
2025-03-26 22:47:09 +0200

    initial commit

---
+++ resources/js/components/AppLogoIcon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+
+defineOptions({
+    inheritAttrs: false,
+});
+
+interface Props {
+    className?: HTMLAttributes['class'];
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <img src="images/logo-icon.png" alt="Logo" class="h-8 w-8" />
+</template>
